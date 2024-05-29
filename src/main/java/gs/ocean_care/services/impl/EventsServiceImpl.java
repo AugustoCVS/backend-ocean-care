eef0f9605396bc68831a0747fe0b3bcf74a51576
AugustoCVS <guty.vertolis@gmail.com>
2024-05-29 13:59:39 -0300

    chore: remove unused import

--- src/main/java/gs/ocean_care/services/impl/EventsServiceImpl.java
+++ src/main/java/gs/ocean_care/services/impl/EventsServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public class EventsServiceImpl implements EventsService {
 
     @Autowired
